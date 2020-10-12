0a8fe729980b67cc023291f48f1af7f6543a4f4f
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2020-10-12 16:29:23 +0900

    ルーティング設定

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,2 +1,5 @@
 class CalendarsController < ApplicationController
+  def index
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  
+  resources :calendars
+  root to: "calendars#index"
 end
