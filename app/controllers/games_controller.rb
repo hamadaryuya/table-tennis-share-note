4dc736a16c74c8f3dee321ac656513433cac7922
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2020-10-19 15:41:33 +0900

    コントローラーのbefore_action設定

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,9 @@
 class GamesController < ApplicationController
-  def new
-  end
+  before_action :game_comment_set, only: [:edit, :show]
 
   def create
     Game.create(game_params)
     redirect_to "/"
-  end
-
-  def edit
-    @game = Game.find(params[:id])
-    @game_comment = GameComment.new
-    @game_comments = @game.game_comments.includes(:user)
-  end
-
-  def show
-    @game = Game.find(params[:id])
-    @game_comment = GameComment.new
-    @game_comments = @game.game_comments.includes(:user)
   end
 
   def update
@@ -32,11 +19,19 @@ class GamesController < ApplicationController
      end
   end
 
+  private
+
   def game_params
     params.permit(:date, :opponent, :result, :good_point, :game_introspection).merge(user_id: current_user.id)
   end
 
   def update_game_params
     params.require(:game).permit(:date, :opponent, :result, :good_point, :game_introspection).merge(user_id: current_user.id)
+  end
+
+  def game_comment_set
+    @game = Game.find(params[:id])
+    @game_comment = GameComment.new
+    @game_comments = @game.game_comments.includes(:user)
   end
 end
--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,22 +1,9 @@
 class TrainingsController < ApplicationController
-  def new
-  end
+  before_action :comment_set, only: [:edit, :show]
 
   def create
     Training.create(training_params)
     redirect_to "/"
-  end
-
-  def edit
-    @training = Training.find(params[:id])
-    @comment = Comment.new
-    @comments = @training.comments.includes(:user)
-  end
-
-  def show
-    @training = Training.find(params[:id])
-    @comment = Comment.new
-    @comments = @training.comments.includes(:user)
   end
 
   def update
@@ -32,11 +19,19 @@ class TrainingsController < ApplicationController
      end
   end
 
+  private
+
   def training_params
     params.permit(:date, :training_menu, :purpose, :introspection).merge(user_id: current_user.id)
   end
 
   def update_training_params
     params.require(:training).permit(:date, :training_menu, :purpose, :introspection).merge(user_id: current_user.id)
+  end
+
+  def comment_set
+    @training = Training.find(params[:id])
+    @comment = Comment.new
+    @comments = @training.comments.includes(:user)
   end
 end
