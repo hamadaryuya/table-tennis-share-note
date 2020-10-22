11ee27cced62b6f6ae1ddc688681bc7cc3bc829e
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2020-10-22 17:48:13 +0900

    CSSの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#create' do
+  describe 'ユーザーの新規登録' do
     before do
       @user = FactoryBot.build(:user)
     end
-
+    
     it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
       expect(@user).to be_valid
     end
